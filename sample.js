373ca92ff264df8afa9f82fdb01bf00702439bb1
repo <reporepo254@gmail.com>
2022-04-02 16:42:37 +0900

    Initial commit

--- sample.js
+++ sample.js
@@ -1,5 +1,5 @@
 var map = L.map('map', {
-    center: [35.65057785972163, 139.74420090103902],
+    center: [35.6, 139.7],
     zoom: 14,
 });
 
@@ -8,13 +8,7 @@ var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 });
 tileLayer.addTo(map);
 
-var memos = [
-    {
-        "lat":35,
-        "lng":140,
-        "memo":"hhh",
-    }
-]
+var memos = []
 var marker_list =[]
 
 var LeafIcon = L.Icon.extend({
@@ -32,24 +26,40 @@ var greenIcon = new LeafIcon({iconUrl: 'leaf-green.png'}),
     redIcon = new LeafIcon({iconUrl: 'leaf-red.png'}),
     orangeIcon = new LeafIcon({iconUrl: 'leaf-orange.png'});
 
-putmuker()
+function init(){
+    const local = localStorage.getItem('memos');
+    const memosJson = JSON.parse(local);
+    console.log(memosJson);
+
+    for (let index = 0; index < memosJson.length; index++) {
+        console.log(memosJson[index])
+        memos.push(memosJson[index])
+    }
+    console.log(memos)
+    putmuker()
+}init()
 
 map.on('click',function(e){
-    var addplace = {lat:e.latlng.lat,lng:e.latlng.lng,memo:"at"}
+    var memo_id = memos.length + 1
+    var addplace = {id:memo_id,lat:e.latlng.lat,lng:e.latlng.lng,memo:""}
     memos.push(addplace)
-    console.log(memos)
     remove()
     putmuker()
 });
 
+
 function putmuker(){
     marker_list.length = 0;
     for (var index = 0; index < memos.length; index++) {
-        var h1 =`<p>${memos[index].memo}</p>`
-        var marker = L.marker([memos[index].lat,memos[index].lng]).addTo(map).bindPopup(h1,({maxWidth:1000}))
+        var v = memos[index].memo
+        var content =`
+        <textarea onkeyup = edit(${index},this.value)
+        id = ${index}
+        value=${v}>${v}</textarea>
+        `
+        var marker = L.marker([memos[index].lat,memos[index].lng]).addTo(map).bindPopup(content,({maxWidth:1000})).openPopup();
         marker_list.push(marker)
     }
-    console.log(marker_list)
 }
 
 function remove () {
@@ -57,4 +67,17 @@ function remove () {
         map.removeLayer(marker_list[index]);
     }
 }
+
+function edit(i,ele) {
+    console.log(memos[i].memo = ele)
+    console.log(memos)
+
+    const memos_String = JSON.stringify(memos);
+    localStorage.setItem('memos',memos_String);
+}
+
+map.on('popupopen', function(e) {
+    var result = memos.filter(memos => memos.lat === e.popup._latlng.lat,memos.lng ===e.popup._latlng.lng)
+    document.getElementById(result[0].id - 1).value = memos[result[0].id - 1].memo
+});
 
